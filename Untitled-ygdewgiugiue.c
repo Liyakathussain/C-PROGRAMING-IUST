64e81dd24d590208d8fc8891a0f6f0f535f286a5
Liyakathussain <137596807+Liyakathussain@users.noreply.github.com>
2023-09-22 09:32:45 +0530

    Add files via upload

---
+++ Untitled-ygdewgiugiue.c
@@ -0,0 +1,13 @@
+//area of rectangle
+#include<stdio.h>
+int main()
+{
+    int length,breadth,area;
+    printf("enter the length");
+    scanf("%d",&length);
+    printf("enter the breadth");
+    scanf("%d",&breadth);
+    area=length*breadth;
+    printf("area is %d",area);
+    return 0;
+}
