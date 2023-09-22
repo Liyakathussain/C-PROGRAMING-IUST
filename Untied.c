0fe11bca8ea00b80e1fd2d990fed4198e2a55044
Liyakathussain <137596807+Liyakathussain@users.noreply.github.com>
2023-09-22 09:40:35 +0530

    Add files via upload

---
+++ Untied.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("enter no.");
+    scanf("%d",&x);
+    if (x>=0)
+    printf("square root will be real");
+    else printf("square root will be complex");
+    return 0;
+}
