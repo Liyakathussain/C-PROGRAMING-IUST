937f12d9e85d367f1a5828ebdf62390af5fd97ad
Liyakathussain <137596807+Liyakathussain@users.noreply.github.com>
2023-09-23 21:37:07 +0530

    Add files via upload

---
+++ twofunction.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Function prototype
+void printHello();
+void printGoodbye();
+
+int main() {
+    printHello(); // Calling the function//
+    printGoodbye();
+    return 0;
+}
+
+// Function definition 
+void printHello() {
+    printf("Hello!\n");
+    }
+void printGoodbye{
+printf("Goodbye");
+    }
