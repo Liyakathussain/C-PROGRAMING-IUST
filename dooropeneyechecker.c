40663a489982ed5a1f11d733a72fd2e315626937
liyakat <137596807+Liyakathussain@users.noreply.github.com>
2023-09-29 09:42:35 +0530

    Add files via upload

---
+++ dooropeneyechecker.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+
+int main(){
+    char D;
+    printf("the D is %c",D);
+    int i;
+    char eyescan;
+    if( i==1){
+        printf("the eyescan is valid open the door");
+    }
+    elseif(i>=3);
+        printf("the eyescan is not valid don't open the door");
+    printf("work according condition is %c",D);
+    
+
+    return 0;
+}
