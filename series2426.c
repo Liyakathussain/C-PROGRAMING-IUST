f792b39d6d9a9e99ccc85a17fd1eaeb496aa8600
liyakat <137596807+Liyakathussain@users.noreply.github.com>
2023-09-30 21:55:15 +0530

    Add files via upload

---
+++ series2426.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main() {
+    int n,i;
+    printf("enter any number");
+    scanf("%d",&n);
+     printf("series");
+     {
+     int product=2;}
+    for
+    (int i=2; i<=n ;i++)
+    { ( int product= product*i );
+    }
+    printf("%d", &i );
+    return 0; 
+    }
