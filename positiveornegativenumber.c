4be61cdde569928d6d4b64a9d9b48cb62d52cb24
liyakat <137596807+Liyakathussain@users.noreply.github.com>
2023-10-01 21:34:48 +0530

    Add files via upload

---
+++ positiveornegativenumber.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x;
+    printf("enter any number");
+    scanf("%d", &x);
+    x>0? printf("positive"):printf("non_positive");
+    getch ();
+} 
