6fa121e3cf71d07fcdd98bad572eda059334d34f
liyakat <137596807+Liyakathussain@users.noreply.github.com>
2023-10-01 21:39:55 +0530

    Add files via upload

---
+++ areasquare.c
@@ -0,0 +1,11 @@
+//area of a rectangle
+#include<stdio.h>
+int main()
+{
+    int side,area;
+    printf("enter the side");
+    scanf("%d",&side);
+    area=side*side;
+    printf("area is %d", area);
+    return 0;
+}
