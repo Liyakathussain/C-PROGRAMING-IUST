dd3086aa323da723c29a0229f3b3e6d301c640ea
liyakat <137596807+Liyakathussain@users.noreply.github.com>
2023-10-03 18:53:05 +0530

    Add files via upload

---
+++ fxngoodbye.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void printhello();
+void printgoodbye();
+int main()
+{
+    printhello();
+    printgoodbye();
+    printhello();
+    printgoodbye();
+    printhello();
+    printgoodbye();
+return 0; 
+} 
+void printhello(){
+    printf("hello\n");
+}
+void printgoodbye()
+{
+    printf("goodbye \n");
+}
