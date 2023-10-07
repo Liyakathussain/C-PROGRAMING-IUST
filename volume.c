2242b103cb3c006f0cca059fc109fdc0447167bf
liyakat <137596807+Liyakathussain@users.noreply.github.com>
2023-10-08 00:57:51 +0530

    Add files via upload

---
+++ volume.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+ #include<math.h>
+int main(){
+    int side;
+    printf("enter the value of side:\n");
+    scanf("%d",&side);
+    printf("the value of area is %f ",pow(side , 2));
+     int a;
+     printf("\n Enter the  value of a :\n");
+     scanf("%d",&a);
+     printf("the value of volume is %f\n",pow(side , 4));
+
+    
+    return 0;
+}
